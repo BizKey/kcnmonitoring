7f511f836fba224c586750b804615a8f7fe08ff3
kizianov.ao <kizianov.ao@svrauto.ru>
2025-10-03 07:29:01 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ async fn main() -> Result<(), JobSchedulerError> {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     let pool = PgPoolOptions::new()
-        .max_connections(5)
+        .max_connections(10)
         .connect(&database_url)
         .await
         .expect("Failed to create pool");
