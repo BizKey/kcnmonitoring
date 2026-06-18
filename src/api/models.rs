0482f6f666ec3fc1646102d13d9eee24b9850a15
kizianov.ao <kizianov.ao@svrauto.ru>
2026-06-18 12:29:58 +0000

    .

--- src/api/models.rs
+++ src/api/models.rs
@@ -38,6 +38,8 @@ pub struct Currencies {
     #[serde(rename = "fullName")]
     pub full_name: String,
 
+    pub precision: u64,
+
     #[serde(rename = "isMarginEnabled")]
     pub is_margin_enabled: bool,
 
